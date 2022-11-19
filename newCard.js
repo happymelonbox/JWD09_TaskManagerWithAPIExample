ebbdd8295103f5b867e6fbc93af35e67df3bd0d9
Justin Berger Howes <howes.j.j@gmail.com>
2022-11-20 02:37:24 +1100

    added cards to home home page

--- newCard.js
+++ newCard.js
@@ -1,9 +1,41 @@
-function createNewCard(){
-
-    // Full new task card
+function createNewCard(obj){
     const cardContainer = document.getElementById("currentTasksContainer")
     const newCard = cardContainer.appendChild(document.createElement("div"))
-    newCard.setAttribute(".card")
+    newCard.setAttribute("class", "card")
     const newCardBody = newCard.appendChild(document.createElement("div"))
-    newCardBody.setAttribute(".card-body")
+    newCardBody.setAttribute("class", "card-body")
+    const cardTitle = newCardBody.appendChild(document.createElement("h5"))
+    cardTitle.setAttribute("class", "card-title")
+    cardTitle.innerHTML = `${obj.cardTitle}`
+    const cardDueDate = newCardBody.appendChild(document.createElement("h6"))
+    cardDueDate.setAttribute("class", "card-subtitle")
+    cardDueDate.setAttribute("class", "mb-2")
+    cardDueDate.setAttribute("class", "text-muted")
+    cardDueDate.innerHTML = `Due Date: ${obj.date}`
+    const cardDescription = newCardBody.appendChild(document.createElement("h6"))
+    cardDescription.setAttribute("class", "card-subtitle")
+    cardDescription.setAttribute("class", "mb-2")
+    cardDescription.setAttribute("class", "text-muted")
+    cardDescription.innerHTML = `Description: ${obj.description}`
+    const cardAssignedTo = newCardBody.appendChild(document.createElement("h6"))
+    cardAssignedTo.setAttribute("class", "card-subtitle")
+    cardAssignedTo.setAttribute("class", "mb-2")
+    cardAssignedTo.setAttribute("class", "text-muted")
+    cardAssignedTo.innerHTML = `Description: ${obj.assigned_to}`
+    const cardStatus = newCardBody.appendChild(document.createElement("h6"))
+    cardStatus.setAttribute("class", "card-subtitle")
+    cardStatus.setAttribute("class", "mb-2")
+    cardStatus.setAttribute("class", "text-muted")
+    cardStatus.innerHTML = `Description: ${obj.status}`
+    const cardPodName = newCardBody.appendChild(document.createElement("h6"))
+    cardPodName.setAttribute("class", "card-subtitle")
+    cardPodName.setAttribute("class", "mb-2")
+    cardPodName.setAttribute("class", "text-muted")
+    cardPodName.innerHTML = `Description: ${obj.pod_name}`
+    const deleteButton = newCardBody.appendChild(document.createElement("button"))
+    deleteButton.innerHTML = "Delete"
+    const editButton = newCardBody.appendChild(document.createElement("button"))
+    editButton.innerHTML = "Edit"
 }
+
+  export default createNewCard
--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 import TaskManager from "./TaskManager.js";
+import createNewCard from "./newCard.js";
 
 window.addEventListener('load', bodyFunction)
 
@@ -9,23 +10,13 @@ function bodyFunction(){
         localStorage.setItem("tasks", JSON.stringify(info))
     })
 
-    
-
     let tasks = localStorage.getItem("tasks")
     const taskContainer = document.getElementById("currentTasksContainer")
-    
+
     JSON.parse(tasks).map(task => {
-        taskContainer.appendChild(document.createElement("p")).innerHTML = `Task Id: ${task.id}<br/>Task Title: ${task.title}<br/>Task Description: ${task.description}<br/>Task Assignment: ${task.assigned_to}<br/>Task Due Date: ${task.date}<br/>Task Status: ${task.status}`
+        createNewCard(task)
     })
 
     // let newTask = new TaskManager("Sharpen Pencil", "Blunt pencils need sharpening", "Steven","2022-11-19", "Done", "Podlet of fire")
     // newTask.saveNewTask()
 }
-
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="card-link">Card link</a>
-    <a href="#" class="card-link">Another link</a>
-  </div>
