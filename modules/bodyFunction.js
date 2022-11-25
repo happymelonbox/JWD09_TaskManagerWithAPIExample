5f483cdaa5773c6498bd8eb370dd6420647abb47
Justin Berger Howes <howes.j.j@gmail.com>
2022-11-26 02:00:02 +1100

    added eventlistener to submit button click

--- modules/bodyFunction.js
+++ modules/bodyFunction.js
@@ -6,7 +6,7 @@ import displayTasks from "./displayTasks.js"
 function bodyFunction(){
     fetchTasks();
     displayTasks();
-    
+    addValidationToModal();
 
     // let newTask = new TaskManager("Sharpen Pencil", "Blunt pencils need sharpening", "Steven","2022-11-19", "Done", "Podlet of fire")
     // newTask.saveNewTask()
